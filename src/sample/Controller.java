fc69a86523cebe37fe91ab7250010d386ff434d5
fayyozbek <pazilovfa@gmail.com>
2021-01-07 15:49:37 +0500

    new functions

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,12 +7,11 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
+
 import java.sql.ResultSet;
 
 public class Controller {
@@ -20,29 +19,34 @@ public class Controller {
     TextField txtLogin;
     @FXML
     TextField txtPassword;
-
+    @FXML
+    Label lblInfo;
 
     @FXML
-    private void onLogin (ActionEvent event) throws Exception{
-        DatabaseConnection databaseConnection=new DatabaseConnection();
-        ResultSet getStmt= databaseConnection.getStmt("Users");
+    private void onLogin(ActionEvent event) throws Exception {
+        ResultSet getStmt = DatabaseConnection.getStmt("Users");
+        if (txtLogin.getText() != null && txtPassword.getText() != null) {
+            while (getStmt.next()) {
 
-        while (getStmt.next()){
-            System.out.println(getStmt.getString("Login"));
-            System.out.println(txtLogin.getText());
-            System.out.println(getStmt.getString("Password"));
-            System.out.println(txtPassword.getText());
-            if (getStmt.getString("Login").equals(txtLogin.getText())  && txtPassword.getText().equals(getStmt.getString("Password"))   ){
-                Parent root = FXMLLoader.load(getClass().getResource("librarians.fxml"));
-                Scene tableView = new Scene(root);
-                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                stage.setScene(tableView);
-                stage.show();
+                if (getStmt.getString("Login").equals(txtLogin.getText()) && txtPassword.getText().equals(getStmt.getString("Password"))) {
+
+                    if (getStmt.getInt("Role") == 0) {
+                        Parent root = FXMLLoader.load(getClass().getResource("librarians.fxml"));
+                        Scene tableView = new Scene(root);
+                        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                        stage.setScene(tableView);
+                        stage.show();
+                    }
+                    break;
+                } else {
+                    lblInfo.setText("Wrong login and password");
+
+                }
+
+
             }
-            else{
-                txtLogin.setText(txtPassword.getText());
-            }
+
+
         }
-
     }
 }
--- src/sample/DatabaseConnection.java
+++ src/sample/DatabaseConnection.java
@@ -6,12 +6,48 @@ public class DatabaseConnection {
     private static Connection con;
 
     public DatabaseConnection() throws SQLException {
-        con=DriverManager.getConnection("jdbc:derby:./db;", "user", "pass");;
+            }
+
+    public static Connection getCon() throws SQLException {
+        con=DriverManager.getConnection("jdbc:derby:./db;", "user", "pass");
+        return con;
     }
 
     public static ResultSet getStmt(String table) throws SQLException {
-        PreparedStatement getPStmt= con.prepareStatement("SELECT * FROM "+table);
-        ResultSet get=getPStmt.executeQuery() ;
+        PreparedStatement getStmt= getCon().prepareStatement("SELECT * FROM "+table);
+        ResultSet get=getStmt.executeQuery() ;
+        con.close();
+
         return get;
     }
+    public static  ResultSet searchStmt (String table, String where, String what)throws Exception{
+        PreparedStatement getStmt= getCon().prepareStatement("SELECT * FROM "+table+" WHERE "+where+" = '"+what+"'");
+        ResultSet get=getStmt.executeQuery() ;
+        con.close();
+        return get;
+    }
+    public static  ResultSet searchStmt (String table, String where, int what)throws Exception{
+        PreparedStatement getStmt= getCon().prepareStatement("SELECT * FROM "+table+" WHERE "+where+" = "+what+"");
+        ResultSet get=getStmt.executeQuery() ;
+        con.close();
+        return get;
+    }
+    public static  boolean deleteStmt(String table, String where, String what) throws  Exception{
+        PreparedStatement stmt= getCon().prepareStatement("DELETE FROM "+table+" WHERE "+where+" = '"+what+"'");
+        boolean deleted= stmt.execute();
+        con.close();
+        return  deleted;
+    }
+    public static  boolean deleteStmt(String table, int id) throws  Exception{
+        boolean deleted= false;
+        if(table.equals("Users")){
+            PreparedStatement stmt= getCon().prepareStatement("DELETE FROM Users WHERE UsersId = "+id+"");
+             deleted= stmt.execute();
+        }
+        con.close();
+        return  deleted;
+    }
+
+
+
 }
